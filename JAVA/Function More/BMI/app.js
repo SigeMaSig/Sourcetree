47536ccadf23439254ee660e167ee90024bf909f
SigeMaSige <sigema.soulerror@gmail.com>
2024-03-26 12:07:13 +0700

    แก้แล้วววววว

--- JAVA/Function More/BMI/app.js
+++ JAVA/Function More/BMI/app.js
@@ -1,36 +1,26 @@
 const bmiarray = {
-    DataUser: [],
-    InputData(data) {
-        if (typeof data == 'number' ) {
-            this.DataUser.push(data);
+    values: [],
+    input(values) {
+        if (values) {
+            this.values.push(parseFloat(values));
         }
-
     },
-    calculateBMI() {
-        if (this.DataUser.length === 2) { 
-            const height = this.DataUser[1] / 100;
-            const weight = this.DataUser[0];
-            const bmi = weight / (height ** 2);
+    SumBMI() {
+        if (this.values.length === 2) {
+            const weight = this.values[0];
+            const height = this.values[1];
+            const bmi = weight / ((height / 100) ** 2);
             return bmi.toFixed(2);
         } else {
-            return null;
+            return "อีสัสเอ้ยยยยยยยย";
         }
-    },
-};
-
-function InputByUser() {
-    const weightDataUser = parseFloat(document.getElementById("weight").value);
-    const heightDataUser = parseFloat(document.getElementById("height").value);
-
-    bmiarray.InputData(weightDataUser);
-    bmiarray.InputData(heightDataUser);
-
-    const bmi = bmiarray.calculateBMI();
-    if (bmi !== null) {
-        document.getElementById("output").value = bmi;
-    } else if (bmi == null){
-        document.getElementById("output").value = "ใส่ค่ามาไม่ครบ";
-       
     }
-    bmiarray.DataUser = []
+};
+function HolyShit(){
+bmiarray.input(document.getElementById("weight").value);
+bmiarray.input(document.getElementById("height").value);
+
+const total = bmiarray.calculateBMI();
+document.getElementById("output").value = total
+bmiarray.values = []
 }
